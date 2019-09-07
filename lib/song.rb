dbf94c5eeb4f6937eb07ff6724f0329e8b3f1b15
Maverick Adolfo <merkonical@gmail.com>
2019-09-07 03:29:46 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -33,7 +33,8 @@ class Song
       if genre_count[genre]
         genre_count[genre] += 1 
       else
-        genre_count[genre] = 1 
+        genre_count[genre] = 1
+      end 
   end 
   
   def self.artist_count 
