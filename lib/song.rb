ebb65a98a36dc7b88881361cf5c243bd9a4af23d
Maverick Adolfo <merkonical@gmail.com>
2019-09-07 03:33:29 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -39,6 +39,30 @@ class Song
     genre_count
   end 
   
-  def self.artist_count 
+  def self.artist_count
+    artist_count = {}
+    @@artists.each do |artist|
+      if artist_count[artist]
   end 
 end 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
